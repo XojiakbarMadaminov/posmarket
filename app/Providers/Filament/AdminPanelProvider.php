5ded9455e61a0d3386e3b7fb947e9680015cde9e
xojiakbar <x.madaminov@uzinfocom.uz>
2025-06-04 15:23:19 +0500

    stats

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,8 +28,9 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::hex('#484ab5'), // Color::hex('#b5972d') // Color::Amber,
             ])
+            ->darkMode()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([

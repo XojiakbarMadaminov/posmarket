41586d4a12f2b3f72c5d640ee04c766cc35532cf
Khojiakbar_back <xmadaminov6635@gmail.com>
2025-06-12 19:35:03 +0500

    barcode c128

--- resources/views/filament/components/barcode.blade.php
+++ resources/views/filament/components/barcode.blade.php
@@ -1,1 +1,11 @@
-<div>{!! DNS1D::getBarcodeHTML($getRecord()->barcode, 'EAN13', 1.4, 40) !!}</div>
+@php
+    $barcode = $getRecord()->barcode;
+@endphp
+
+@if(preg_match('/^[\w\-]+$/', $barcode))
+    {{-- Harf/raqam/– bo‘lsa: CODE128 bilan ko‘rsatiladi --}}
+    <div>{!! DNS1D::getBarcodeHTML($barcode, 'C128', 2, 40) !!}</div>
+@else
+    {{-- Yaroqsiz bo‘lsa: oddiy matn --}}
+    <div class="text-gray-700 font-semibold">{{ $barcode }}</div>
+@endif
